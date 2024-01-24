f031170cb1c1f81ade318aa6bf275e053bc78c44
ainooson <ojidaddy@gmail.com>
2024-01-23 12:47:33 -1200

    This is Activity 0 REACT Demo

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Hello, From React World!</h1>
+      </header>
+      <main className="main">
+        <img src="https://bit.ly/4834cGw" className="photo" alt="Pexels" />
+        <a
+          className="link"
+          href="https://pexels.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Free Stock Photos from Pexels ☀️ ❤️ 🫶 🔥 ✅ ✨ 😊 🚀
+        </a>
+        <p>
+          Learn React, or you won't drink <span>Bissap</span> again
+        </p>
+      </main>
+    </div>
+  );
+}
+export default App;
